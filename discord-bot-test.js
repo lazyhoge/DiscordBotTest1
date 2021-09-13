b7775181f1e5868ce390badc510945efba7bc990
DESKTOP-MAIN\ryota <reg@lazyhoge.com>
2021-09-13 21:49:14 +0900

    no message

--- discord-bot-test.js
+++ discord-bot-test.js
@@ -1,12 +1,8 @@
-//configを読み込む
-const config = require('./config.json');
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
 
-// Discord.jsモジュールを読み込む
-const Discord = require('discord.js');
-
-// 新しいDiscordクライアントを作成
-const client = new Discord.Client({intents: [Intents.FLAGS.GUILDS]});
-
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 //機密情報の書かれた.envを読み込む
 require('dotenv').config();
 

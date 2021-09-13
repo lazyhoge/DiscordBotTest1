5d55c8dad3d889e9888e1ebe7ef653db1f46d815
DESKTOP-MAIN\ryota <reg@lazyhoge.com>
2021-09-13 22:05:55 +0900

    no message

--- discord-bot-test.js
+++ discord-bot-test.js
@@ -3,6 +3,7 @@ const { Client, Intents } = require('discord.js');
 // 新しいDiscordクライアントを作成
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
+require('dotenv').config();
 
 // クライアントの準備ができた際に実行されます
 // このイベントはログインした後に１度だけ実行します

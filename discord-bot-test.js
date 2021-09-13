41dbc7bda4cbd20fde8fd52c095cc2412124b971
DESKTOP-MAIN\ryota <reg@lazyhoge.com>
2021-09-13 21:59:56 +0900

    no message

--- discord-bot-test.js
+++ discord-bot-test.js
@@ -1,10 +1,8 @@
-// Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
+// Discord.jsモジュールを読み込む
+const Discord = require('discord.js');
 
-// Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-//機密情報の書かれた.envを読み込む
-require('dotenv').config();
+// 新しいDiscordクライアントを作成
+const client = new Discord.Client();
 
 // クライアントの準備ができた際に実行されます
 // このイベントはログインした後に１度だけ実行します
@@ -12,16 +10,5 @@ client.once('ready', () => {
 	console.log('準備完了！');
 });
 
-//メッセージを待ち受ける
-client.on('message', message => {
-	console.log(message.content);
-});
-
-//メッセージを送信
-if (message.content === '!ping') {
-	// メッセージが送信されたチャンネルへ「Pong.」を送り返す。
-	message.channel.send('Pong.');
-}
-
 // トークンを使ってDiscordにログイン
-client.login(prossess.env.DiscordBotTest_token);
+client.login('トークンをここに貼り付ける');

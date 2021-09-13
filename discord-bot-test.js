031ecf77c05e2bee365388e3e86d29bc76deec40
DESKTOP-MAIN\ryota <reg@lazyhoge.com>
2021-09-13 20:27:47 +0900

    add !ping command

--- discord-bot-test.js
+++ discord-bot-test.js
@@ -1,12 +1,31 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
+//.envを読み込めるようにする
 require('dotenv').config();
 
+//configを読み込む
+const config = require('./config.json');
+
+// Discord.jsモジュールを読み込む
+const Discord = require('discord.js');
+
+// 新しいDiscordクライアントを作成
+const client = new Discord.Client();
+
+// クライアントの準備ができた際に実行されます
+// このイベントはログインした後に１度だけ実行します
 client.once('ready', () => {
 	console.log('準備完了！');
 });
 
+//メッセージを待ち受ける
+client.on('message', message => {
+	console.log(message.content);
+});
 
-client.login(process.env.DiscordBotTest_token);
-//iidesune2
+//メッセージを送信
+if (message.content === '!ping') {
+	// メッセージが送信されたチャンネルへ「Pong.」を送り返す。
+	message.channel.send('Pong.');
+}
+
+// トークンを使ってDiscordにログイン
+client.login(prossess.env.DiscordBotTest_token);
